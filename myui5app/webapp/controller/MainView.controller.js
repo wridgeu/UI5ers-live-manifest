ddcc5b5d2a9883cfea898daa08b795e5152f6435
Marco Beier <marco-beier@t-online.de>
2025-02-02 10:19:22 +0100

    Initialize repository with easy-ui5

---
+++ myui5app/webapp/controller/MainView.controller.js
@@ -0,0 +1,15 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller) {
+        "use strict";
+
+        return Controller.extend("myui5app.controller.MainView", {
+            onInit: function () {
+
+            }
+        });
+    });
